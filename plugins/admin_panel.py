6d33f9b9ba60c28bf72e79356274c3a4a71ae0cf
ROHIT12-bit <reddyrohit823@gmail.com>
2025-11-18 19:31:54 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -36,8 +36,8 @@ async def tutorial(bot,message):
 	    text =Txt.FILE_NAME_TXT.format(format_template=format_template),
 	    disable_web_page_preview=True,
 	    reply_markup=InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Admin", url = "https://t.me/DvisDmBot"), 
-        			InlineKeyboardButton("⚡ Tutorial", url = "https://t.me/net_pro_max") ]])
+        			[InlineKeyboardButton("🦋 Admin", url = "https://t.me/Funnytamilan"), 
+        			InlineKeyboardButton("⚡ Tutorial", url = "https://t.me/Funnytamilan") ]])
 	)
 
 
@@ -95,6 +95,7 @@ async def send_msg(user_id, message):
     except Exception as e:
         logger.error(f"{user_id} : {e}")
         return 500
+
 
 
 
